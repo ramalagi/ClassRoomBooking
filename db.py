d9f4149b72b9e041c3c0173a3de2524af3d74600
Suman Naikar <suman.naikar@example.com>
2026-05-10 21:59:00 +0530

    fixed deployment issue

--- db.py
+++ db.py
@@ -1,6 +1,13 @@
-import sqlite3
 import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 
 def get_db_connection():
-    db_path = os.getenv('DATABASE_URL', 'roombooking.db')
-    return sqlite3.connect(db_path)
+    return psycopg2.connect(
+        DATABASE_URL,
+        cursor_factory=RealDictCursor
+    )
